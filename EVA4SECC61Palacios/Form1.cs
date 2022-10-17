e54a6bdf685b1d56560714e7e14c51e6eaebd0ca
DIAN <DIAN@DESKTOP-MAIQ2VO>
2022-10-17 12:50:34 -0300

    Agregar archivos de proyecto.

---
+++ EVA4SECC61Palacios/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EVA4SECC61Palacios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // holi... leo
+
+        // lo vesss?
+        //siiii, es una tortuga esto si en un principio por lo visto
+
+    }
+}
